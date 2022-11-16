23c01ec9974f34246ef7c8ddc0c1b2e1f1acf30f
ahmedelzaky <ahmed.elzakistudy@gmail.com>
2022-11-16 22:54:26 +0200

    Version 2.0

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ let observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
       entry.target.classList.add("show");
-    } else {
-      entry.target.classList.remove("show");
-    }
+    // } else {
+    //   entry.target.classList.remove("show");
+    // }
   });
 });
 
