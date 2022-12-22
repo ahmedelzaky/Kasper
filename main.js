7f11be3b14d80eb6348216aa8023b32eb1d44dcf
ahmedelzaky <ahmed.elzakistudy@gmail.com>
2022-12-22 23:05:03 +0200

    v2.1

--- main.js
+++ main.js
@@ -12,3 +12,17 @@ let observer = new IntersectionObserver((entries) => {
 });
 
 hiddenElments.forEach((el) => observer.observe(el));
+
+let mButton = document.querySelector(".toggle-menu");
+let menu = document.querySelector(".m_menu");
+let displayMenu = false;
+
+mButton.addEventListener("click", function () {
+  if (!displayMenu) {
+    menu.style.display = "flex";
+    displayMenu = true;
+  } else {
+    menu.style.display = "none";
+    displayMenu = false;
+  }
+});
