7cea60869fd821e9b0d5737588f1f593be12d1e1
ahmedelzaky <ahmed.elzakistudy@gmail.com>
2023-07-01 22:22:00 +0300

    Add Image Slider

--- main.js
+++ main.js
@@ -1,3 +1,49 @@
+//photo slider
+let photoDir = [
+  "images/slide-01.jpg",
+  "images/slide-02.jpg",
+  "images/slide-03.jpg",
+];
+
+let bullets = document.querySelectorAll(".bullets li");
+
+let landing = document.querySelector(".landing");
+
+let index = Math.floor(Math.random() * photoDir.length);
+console.log(index);
+
+function changePhoto() {
+  if (index > photoDir.length - 1) {
+    index = 0;
+  }
+
+  // Change the background image
+  landing.style.backgroundImage = `url(${photoDir[index]})`;
+
+  // Update active bullet
+  bullets[index].classList.add("active");
+  bullets.forEach((bullet) => {
+    if (bullet != bullets[index]) {
+      bullet.classList.remove("active");
+    }
+  });
+
+  index++;
+}
+
+changePhoto();
+
+setInterval(changePhoto, 5000);
+
+//change photo on bullet click
+bullets.forEach((bullet, i) => {
+  bullet.addEventListener("click", function () {
+    index = i;
+    changePhoto();
+  });
+});
+
+//hide and show elements on scroll
 let hiddenElments = document.querySelectorAll(".hidden");
 
 let observer = new IntersectionObserver((entries) => {
